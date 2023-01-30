f35db3450d389b3da0a5269dacea60076c17abbd
kvebiant <kvebiant@gmail.com>
2023-01-30 10:00:41 +0700

    stylizing projects items

--- components/Assets/PorjectAssets/ProjectItems.tsx
+++ components/Assets/PorjectAssets/ProjectItems.tsx
@@ -6,17 +6,29 @@ const ProjectItem = ({
   title,
   subTitle,
   backgroundImg,
+  hasDesc,
   projectUrl,
+  projectDemo,
+  aspectRatio,
 }: {
   title: string;
   subTitle: string;
   backgroundImg: any;
+  hasDesc: boolean;
   projectUrl: string;
+  projectDemo: string;
+  aspectRatio: string;
 }) => {
   return (
-    <div className=" flex hover:scale-105 h-auto w-full shadow-xl shadow-gray-400 hover:shadow-teal-700 rounded-xl p-4 group hover:bg-gradient-to-r from-teal-400 to-teal-600">
+    <div
+      className={
+        hasDesc === true
+          ? " flex mb-5 md:mb-10 hover:scale-105 h-auto w-full shadow-xl shadow-gray-400 hover:shadow-teal-700 rounded-xl p-4 ease-in duration-300 group hover:bg-gradient-to-r from-teal-400 to-teal-600"
+          : " flex mb-5 md:mb-10 hover:scale-105 h-auto w-full shadow-xl shadow-gray-400 hover:shadow-teal-700 rounded-xl p-4 ease-in duration-300"
+      }
+    >
       <Image
-        className="rounded-xl group-hover:opacity-10"
+        className={"rounded-xl w-full group-hover:opacity-10 " + aspectRatio}
         src={backgroundImg}
         alt="404: Image not Found"
         width={400}
@@ -24,15 +36,24 @@ const ProjectItem = ({
         layout="responsive"
         priority
       />
-      <div className=" hidden group-hover:block absolute top-[25%] left-[50%] translate-x-[-50%] translate-y-[50%]">
-        <h3 className="text-2xl text-white text-center">{title}</h3>
-        <p className="pb-4 text-white text-center">{subTitle}</p>
-        <Link href={projectUrl}>
-          <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
-            View Code
-          </p>
-        </Link>
-      </div>
+      {hasDesc === true ? (
+        <div className=" hidden columns-1 group-hover:block absolute items-center justify-center top-[10%] left-[50%] translate-x-[-50%] translate-y-[50%]">
+          <h3 className="text-2xl text-white text-center">{title}</h3>
+          <p className="pb-4 text-white text-center">{subTitle}</p>
+          <Link href={projectUrl}>
+            <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer mb-5">
+              View Code
+            </p>
+          </Link>
+          {projectDemo != "" ? (
+            <Link href={projectDemo}>
+              <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
+                View Demo
+              </p>
+            </Link>
+          ) : null}
+        </div>
+      ) : null}
     </div>
   );
 };
--- components/Sections/Projects.tsx
+++ components/Sections/Projects.tsx
@@ -3,15 +3,50 @@ import ProjectItems from "../Assets/PorjectAssets/ProjectItems";
 
 export default function Projects() {
   return (
-    <section id="Projects" className=" mw-full min-h-[(100vh-5rem)] h-full bg-white mb-4 text-black p-2 flex-auto">
+    <section
+      id="Projects"
+      className=" mw-full min-h-[(100vh-5rem)] h-full bg-white mb-4 text-black p-2 flex-auto"
+    >
       <div className=" max-w-[1240px] text-center mx-auto p-2 flex flex-col justify-center ">
         <div className="text-xl tracking-widest text-teal-700">
           <h2 className="py-4">What I've Built</h2>
           <div className="grid md:grid-cols-2 gap-8">
-            <ProjectItems title='Test Subject' subTitle="React JS" backgroundImg={process.env.NEXT_PUBLIC_PORT as string + '/assets/image/projects/a'} projectUrl='/' />
-            <ProjectItems title='Test Subject' subTitle="React JS" backgroundImg={process.env.NEXT_PUBLIC_PORT as string + '/assets/image/projects/a'} projectUrl='/' />
-            <ProjectItems title='Test Subject' subTitle="React JS" backgroundImg={process.env.NEXT_PUBLIC_PORT as string + '/assets/image/projects/a'} projectUrl='/' />
-            <ProjectItems title='Test Subject' subTitle="React JS" backgroundImg={process.env.NEXT_PUBLIC_PORT as string + '/assets/image/projects/a'} projectUrl='/' />
+            <ProjectItems
+              title="Weather App"
+              subTitle="Flutter"
+              backgroundImg={
+                (process.env.NEXT_PUBLIC_PORT as string) +
+                "/assets/image/projects/a"
+              }
+              aspectRatio="aspect-auto"
+              hasDesc={true}
+              projectUrl="https://github.com/kvebiant/clima"
+              projectDemo=""
+            />
+            <ProjectItems
+              title=''
+              subTitle=''
+              backgroundImg={
+                (process.env.NEXT_PUBLIC_PORT as string) +
+                "/assets/image/projects/a"
+              }
+              aspectRatio="aspect-square"
+              hasDesc={false}
+              projectUrl="/"
+              projectDemo=""
+            />
+            <ProjectItems
+              title=''
+              subTitle=''
+              backgroundImg={
+                (process.env.NEXT_PUBLIC_PORT as string) +
+                "/assets/image/projects/a"
+              }
+              aspectRatio="aspect-square"
+              hasDesc={false}
+              projectUrl="/"
+              projectDemo=""
+            />
           </div>
         </div>
       </div>
