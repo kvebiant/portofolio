d72d5ca29969b37311e622dfcec466aefc46b46a
kvebiant <kvebiant@gmail.com>
2023-01-30 10:45:44 +0700

    fixing assets

--- components/Sections/About.tsx
+++ components/Sections/About.tsx
@@ -8,7 +8,10 @@ import CardSkill from "../Assets/AboutAssets/Skills";
 
 export default function About() {
   return (
-    <section id="About" className=" w-full min-h-[(100vh-5rem)] bg-teal-600 text-white p-2">
+    <section
+      id="About"
+      className=" w-full min-h-[(100vh-5rem)] bg-teal-600 text-white p-2"
+    >
       <div className=" max-w-[1240px] h-full text-center mx-auto p-2 flex flex-col justify-center ">
         <div>
           <h3 className="text-sm pt-4 text-white/90">Get to Know</h3>
@@ -17,7 +20,10 @@ export default function About() {
             <div className=" bg-white rounded-xl p-4">
               <Image
                 className=" rounded-xl hover:scale-90 hover:rotate-6 ease-in duration-300"
-                src={'/assets/image/ava.jpeg'}
+                src={
+                  (process.env.NEXT_PUBLIC_PORT as string) +
+                  "/assets/image/ava.jpeg"
+                }
                 alt="/"
                 width={2000}
                 height={2000}
@@ -27,15 +33,47 @@ export default function About() {
             <div>
               <h3 className=" text-lg py-4">What I Can Do</h3>
               <div className=" grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 border-b-2 py-2">
-                <CardSkill icon={AiFillHtml5} iconColor="text-orange-600" text="HTML" />
+                <CardSkill
+                  icon={AiFillHtml5}
+                  iconColor="text-orange-600"
+                  text="HTML"
+                />
                 <CardSkill icon={DiCss3} iconColor="text-blue-600" text="CSS" />
-                <CardSkill icon={IoLogoJavascript} iconColor="text-yellow-500" text="Javascript" />
-                <CardSkill icon={SiTypescript} iconColor="text-blue-500" text="Typescript" />
-                <CardSkill icon={DiReact} iconColor="text-blue-400" text="React JS" />
-                <CardSkill icon={IoLogoNodejs} iconColor="text-green-700" text="Node JS" />
-                <CardSkill icon={DiMongodb} iconColor="text-green-700" text="MongoDB" />
-                <CardSkill icon={TbBrandNextjs} iconColor="text-black/75" text="Next JS" />
-                <CardSkill icon={TbBrandTailwind} iconColor="text-teal-500" text="Tailwind CSS" />
+                <CardSkill
+                  icon={IoLogoJavascript}
+                  iconColor="text-yellow-500"
+                  text="Javascript"
+                />
+                <CardSkill
+                  icon={SiTypescript}
+                  iconColor="text-blue-500"
+                  text="Typescript"
+                />
+                <CardSkill
+                  icon={DiReact}
+                  iconColor="text-blue-400"
+                  text="React JS"
+                />
+                <CardSkill
+                  icon={IoLogoNodejs}
+                  iconColor="text-green-700"
+                  text="Node JS"
+                />
+                <CardSkill
+                  icon={DiMongodb}
+                  iconColor="text-green-700"
+                  text="MongoDB"
+                />
+                <CardSkill
+                  icon={TbBrandNextjs}
+                  iconColor="text-black/75"
+                  text="Next JS"
+                />
+                <CardSkill
+                  icon={TbBrandTailwind}
+                  iconColor="text-teal-500"
+                  text="Tailwind CSS"
+                />
               </div>
               <p className="py-4">
                 Hi there! My name is Kiki Vebiant from Jakarta, Indonesia and I
